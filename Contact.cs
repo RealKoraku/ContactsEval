b2c36f6b6ac8504236c2f39dfe4737c53c32fbb2
Jacob Kowrach <jakowrach@gmail.com>
2023-06-07 10:44:03 -0500

    Add project files.

---
+++ Contact.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Printing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsAttempt {
+    internal class Contact {
+
+        public string dataId;
+        public string firstName;
+        public string middleName;
+        public string lastName;
+        public string nickname;
+        public string title;
+        public string birthDate;
+        public string email;
+        public string phone;
+        public string street;
+        public string city;
+        public string state;
+        public string zipCode;
+        public string country;
+        public string website;
+        public string notes;
+        public string picture;
+        public bool isFavorite;
+        public bool isActive;
+
+        public string DataId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public string Title { get; set; }
+        public string BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string Website { get; set; }
+        public string Notes { get; set; }
+        public string Picture { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool IsActive { get; set; }
+
+        public Contact(string dataId) {
+
+        }
+
+      //private Contact SetContact() {
+      //    string connectionString = "Server=localhost;Database=Contacts;Trusted_Connection=true";
+
+
+      //}
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactsAttempt {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        static string connectionString = "Server=localhost;Database=Contacts;Trusted_Connection=true";
+
+        public MainWindow() {
+     
+            InitializeComponent();
+
+            bool connected = TestConnection();
+            PopulateListBox();
+        }
+
+        static bool TestConnection() {
+            try {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                connection.Close();
+                return true;
+
+            } catch (Exception exception) {
+                return false;
+            }
+        }
+
+        static void AddContact(string tableName) {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string commandString = $"SELECT * FROM dbo.{tableName}";
+
+            SqlCommand command = new SqlCommand(commandString);
+
+            try {
+                SqlDataReader result = command.ExecuteReader();
+
+                while (result.Read()) {
+                    for (int fieldIndex = 0; fieldIndex < result.FieldCount; fieldIndex++) {
+                        Console.WriteLine(($"{result[fieldIndex]} "));
+                    }
+                }
+
+            } catch (Exception exception) {
+                Console.WriteLine(exception);
+            }
+            connection.Close();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+            NameId.Content = "Hello";
+
+            AddContact("tblContact");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+        }
+
+        private void UpdateContactScreen(Contact currentContact) {
+            NameId.Content = currentContact.FirstName + " " + currentContact.MiddleName + " " + currentContact.LastName;
+            StreetId.Content = currentContact.Street;
+            CityId.Content = currentContact.City;
+            StateId.Content = currentContact.State;
+            ZipId.Content = currentContact.ZipCode;
+            EmailId.Content = currentContact.Email;
+            PhoneId.Content = currentContact.Phone;
+            WebId.Content = currentContact.Website;
+            NotesId.Content = currentContact.Notes;
+
+        }
+
+        private void PopulateListBox() { 
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            ListBox contactsList = new ListBox();
+
+            string commandString = "SELECT firstName FROM dbo.tblContact";
+
+            SqlCommand command = new SqlCommand(commandString, connection);
+
+            try {
+                SqlDataReader result = command.ExecuteReader();
+
+                while (result.Read()) {
+                    for (int fieldIndex = 0; fieldIndex < result.FieldCount; fieldIndex++) {
+                        contactsList.Items.Add(($"{result[fieldIndex]} "));
+                    }
+                }
+
+            } catch (Exception exception) {
+                Console.WriteLine(exception);
+            }
+            connection.Close();
+
+        }
+    }
+}
